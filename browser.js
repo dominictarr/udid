31384e989e26c26bd665a06360627d50c6f00424
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-23 18:15:23 -0500

    fallback to count-tabs in the browser

--- browser.js
+++ browser.js
@@ -1,12 +1,17 @@
 
-var sum = require('sha1sum')
+var tabs = require('count-tabs')()
 
 module.exports = function (name) {
+
+  //the client side will have different domains,
+  //and thus different localStorages per app,
+  //so we don't need to use the name...
+  //however, still use the same api as on the server.
 
   if(!name)
     throw new Error('provide app name')
 
-  return localStorage['UDID-'+name] = localStorage['UDID-'+name] || sum(Date.now())
+  return tabs.id
   
 }
   
